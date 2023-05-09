8b0b2c1e2eeb7df29d8b6cf5d9f4506bdd434f1d
Esther <estherccr@al.insper.edu.com>
2023-05-09 16:26:36 -0300

    check 6- Ana

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Note
 
@@ -6,8 +6,17 @@ from .models import Note
 
 
 def index(request):
-    all_notes = Note.objects.all().order_by('-created_at')
-    return render(request, 'notes/index.html', {'notes': all_notes})
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+
+        print(f'Titulo={title}\nConteudo={content}\n')
+        new_note = Note(title= title , content= content)
+        new_note.save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all().order_by('-created_at')
+        return render(request, 'notes/index.html', {'notes': all_notes})
 
 
 def creditos(request):
